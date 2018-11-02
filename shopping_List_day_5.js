0ad63b76b35ba10097a226c77875c20e12f01c2a
R-Cole <randy@goodglue.com>
2018-11-02 08:48:30 -0700

    Start shopping list file

---
+++ shopping_List_day_5.js
@@ -0,0 +1,38 @@
+function renderShoppingList(){
+
+    //render DOM
+    console.log('`renderShopping` ran');
+
+}
+
+function handleNewItemSubmit(){
+
+    //handle when new items are added by user
+    console.log('`handleNewItemSubmit` ran');
+
+}
+
+function handleItemCheckClicked() {
+
+    //User clicks check button
+    console.log('`handleItemCheckClicked` ran');
+
+    
+}
+
+function handleDeleteitemClicked() {
+
+    //User clicks delete item
+    console.log('`handleDeleteItemClicked` ran');
+    
+}
+
+
+//controller function run all other functions
+function handleShoppingList(){
+
+
+
+}
+
+
