62fbf1d2a6779cb4c8400dd11d760cc55b8d4b7b
R-Cole <randy@goodglue.com>
2018-11-02 09:44:36 -0700

    changes to Shopping list

--- shopping_List_day_5.js
+++ shopping_List_day_5.js
@@ -2,15 +2,81 @@
 const store =[
         {name:"lime",checked:false},
         {name:"banana",checked:false},
-        {name:"apples",checked:false}
+        {name:"apples",checked:false},
+        {name:"soup",checked:false}
 ];
 
 function renderShoppingList(){
 
+    //for each item in STORE generate a string template for <li>
+    //item name rendered as inner text
+
+    const toStringify = templateCreator(store).join("");
+ 
+
+        //items index in store set as data attribute on the <li>
+
+        //items checked state
+
+    //make one string out of stuff
+
+    //insert the string into the DOM 
+
+    console.log(toStringify);
+
+
+    //traverse to shopping list form
+    
+
+    //look for <ul>
+
+
+    $('shopping-list js-shopping-list').html(toStringify);
+
     //render DOM
     console.log('`renderShopping` ran');
 
+    
+
 }
+
+
+function templateCreator(store){
+
+    const tempStore = [];
+
+    for(let i = 0; i < store.length; i++){
+
+        tempStore.push(
+        `
+        <li>
+        <span class="shopping-item shopping-item__checked">${store[i][name]}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button> <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                    </button>
+                </div>
+        </li>;`);
+
+        
+        
+    }
+
+    console.log('hello');
+
+    console.log(tempStore);
+
+    return tempStore;
+
+    
+
+    }
+
+   
+
+
 
 function handleNewItemSubmit(){
 
